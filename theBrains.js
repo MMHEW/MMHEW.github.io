8705f95fb34f93b5baaba80c40b033648ccbad55
MMHEW <92035863+MMHEW@users.noreply.github.com>
2021-10-18 16:12:20 -0400

    Commit 4
    added an alert to the button press

--- theBrains.js
+++ theBrains.js
@@ -1,1 +1,22 @@
-var placeholder = 100;
+  
+var counter = 0;
+
+document.addEventListener('DOMContentLoaded', function(){
+	console.log("This method will be executed once the browser render all the objects ");
+	console.log("Hello world :D");
+})
+
+function Pressed(){
+
+	
+	counter = counter + 1;
+	console.log("The value of counter is " + counter);
+	/*
+	var myContainer = document.getElementById('mycontainer');
+
+	var dynamicparagraph = "<p> Element "+counter+ " added dynamically</p>";
+	myContainer.insertAdjacentHTML('beforeend', dynamicparagraph);
+    */
+
+    alert('that hurt!');
+}
